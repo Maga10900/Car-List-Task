af9a4b958da38f6a361c33d67a6779c811a756f8
Muhammad <maga168123@gmail.com>
2024-04-29 19:17:48 +0400

    Car List Task

---
+++ Command_and_MVVM/MvvmLesson/DataBases/CarDataBase.cs
@@ -0,0 +1,10 @@
+﻿using MvvmLesson.Models;
+using System.Collections.ObjectModel;
+
+namespace MvvmLesson.DataBases;
+
+public static class CarDataBase
+{
+    public static ObservableCollection<Car>? CarsDB { get; set; } = new ObservableCollection<Car>();
+
+}
